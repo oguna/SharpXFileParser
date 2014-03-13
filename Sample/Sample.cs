82eb269bbfb07d31184db77d759b26d01091a579
Administrator <nao.lk.118@gmail.com>
2014-03-13 22:05:02 +0900

    My first commit

---
+++ Sample/Sample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using SharpDX;
+
+
+namespace Sample
+{
+    // Use these namespaces here to override SharpDX.Direct3D11
+    using SharpDX.Toolkit;
+    using SharpDX.Toolkit.Graphics;
+    using SharpDX.Toolkit.Input;
+
+    /// <summary>
+    /// Simple Sample game using SharpDX.Toolkit.
+    /// </summary>
+    public class Sample : Game
+    {
+        private GraphicsDeviceManager graphicsDeviceManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Sample" /> class.
+        /// </summary>
+        public Sample()
+        {
+            // Creates a graphics manager. This is mandatory.
+            graphicsDeviceManager = new GraphicsDeviceManager(this);
+
+            // Setup the relative directory to the executable directory
+            // for loading contents with the ContentManager
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // Modify the title of the window
+            Window.Title = "Sample";
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            // Use time in seconds directly
+            var time = (float)gameTime.TotalGameTime.TotalSeconds;
+
+            // Clears the screen with the Color.CornflowerBlue
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
